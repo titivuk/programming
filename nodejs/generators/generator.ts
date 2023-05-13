eb7666bd1df03051a6751c4f693c0a01a4ff1a14
Kanstantsin Tatarchuk <titivuk@gmail.com>
2023-05-13 17:35:30 +0200

    add generator example

---
+++ nodejs/generators/generator.ts
@@ -0,0 +1,27 @@
+function* generatorFn() {
+  yield "a";
+  yield "b";
+  yield "c";
+  // won't show up in for of or iterator traverse because done = true
+  return "d";
+}
+
+const generator = generatorFn();
+
+/**
+ * generator implements iterator protocol
+ */
+let iterationResult = generator.next();
+while (!iterationResult.done) {
+  console.log(iterationResult.value); // a b c
+  iterationResult = generator.next();
+}
+
+/**
+ * generator implements iterable protocol
+ */
+for (const item of generatorFn()) {
+  console.log(item); // a b c
+}
+
+export {};
